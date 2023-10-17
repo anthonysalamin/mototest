1f98824c86673ea427cfba9fbffee40d90e00d6f
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2023-10-17 15:20:58 +0800

    updated server-less function to implement webflow site fetch API

--- netlify/functions/my-function.js
+++ netlify/functions/my-function.js
@@ -1,8 +1,47 @@
 // netlify/functions/my-function.js
 
+/*
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello from my serverless function!" }),
   };
 };
+*/
+
+exports.handler = async (event, context) => {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      authorization:
+        "Bearer 9cd88f37769012e956a2047194fd68f58e4763d6abf60ab5c68fe7249266660c"
+    }
+  };
+
+  async function fetchData() {
+    try {
+      const response = await fetch("https://api.webflow.com/v2/sites/652cd61429f6f7d9d21bb49e", options);
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      console.error(err);
+      throw err; // You can also throw the error if needed
+    }
+  }
+
+  try {
+    const responseData = await fetchData();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(responseData),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500, // or any appropriate error code
+      body: JSON.stringify({ error: "An error occurred" }),
+    };
+  }
+};
+
